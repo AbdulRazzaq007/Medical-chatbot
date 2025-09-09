ba2eccc2060bd22280f85690cbe619af7a70ba03
Abdul Razzaq <ar087218@gmail.com>
2025-09-09 13:23:34 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Medical-chatbot",
+    version = "0.1.0",
+    author = "AbdulRazzaq",
+    author_email = "ar087218@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
